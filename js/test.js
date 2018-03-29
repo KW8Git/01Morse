b44c219acd61e314a8957d293c517ea8a66360ed
KW8Git <3100658588@qq.com>
2018-03-30 01:03:53 +0800

    add PugongY

--- js/test.js
+++
@@ -1,9 +0,0 @@
-var str = "   acb v j d k ";
-//alert(str.trim());
-//alert(str.split(""));
-//alert(str.split("")[0]==null);//false
-//alert(str.split("")[0]=="");//false
-//alert(str.split("")[0]==" ");//true
-//console.log(str.split(""));
-
-
