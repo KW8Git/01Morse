19c2680035f97f7ec2be10a03547119c60311bdc
KW8Git <3100658588@qq.com>
2018-03-30 14:32:41 +0800

    update snowPugongY.css

---
+++ js/test.js
@@ -0,0 +1,9 @@
+var str = "   acb v j d k ";
+//alert(str.trim());
+//alert(str.split(""));
+//alert(str.split("")[0]==null);//false
+//alert(str.split("")[0]=="");//false
+//alert(str.split("")[0]==" ");//true
+//console.log(str.split(""));
+
+
